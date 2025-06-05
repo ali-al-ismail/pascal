3d7574f17068a5e929524ddbc0bfd5266f21e253
ali-al-ismail <2220005098@iau.edu.sa>
2025-06-05 05:48:33 +0300

    properly scroll through files

--- src/editor.rs
+++ src/editor.rs
@@ -15,7 +15,15 @@ pub struct Editor {
     term: Terminal,
     quit: bool,
     mode: Mode,
-    docu: Vec<String>,
+    docu: Document,
+    cursor_x: u16,
+    cursor_y: u16,
+    top_offset: u16,
+}
+
+struct Document {
+    lines: Vec<String>,
+    n_lines: u16,
 }
 
 impl Editor {
@@ -27,6 +35,9 @@ impl Editor {
             quit: false,
             mode: Mode::NORMAL,
             docu,
+            cursor_x: 0,
+            cursor_y: 0,
+            top_offset: 0,
         })
     }
 
@@ -35,7 +46,7 @@ impl Editor {
             panic!("Couldn't welcome because of: {e}");
         }
         // main editor loop
-        self.render();
+
         loop {
             if self.quit {
                 break;
@@ -78,8 +89,13 @@ impl Editor {
             match self.mode {
                 Mode::NORMAL => {
                     self.handle_normal_mode_key_event(key)?;
+                    Terminal::clear()?;
+                    self.render()?;
                 }
-                _ => {}
+                Mode::INSERT => {
+                    Terminal::clear()?;
+                    self.render()?;
+                }
             }
         }
         // TODO: HANDLE RESIZE EVENT TO RESIZE WIDTH HEIGHT OF TERM
@@ -114,38 +130,71 @@ impl Editor {
     }
 
     // moves cursor based on directional key pressed
-    fn handle_movement(&self, direction: KeyCode) -> Result<(), Error> {
+    fn handle_movement(&mut self, direction: KeyCode) -> Result<(), Error> {
         // TODO: MOVE BASED CURSOR BASED ON DIRECTIONAL KEY
         // WRAP CURSOR IF RIGHT OR LEFT BOUNDS REACHED
         // GO TO TOP / BOTTOM BASED ON UP OR BOTTOM BOUNDS REACHED
         match direction {
             (KeyCode::Char('h') | KeyCode::Left) => {
                 Terminal::move_left(1)?;
+                self.cursor_x = self.cursor_x.saturating_sub(1);
             }
             (KeyCode::Char('j') | KeyCode::Right) => {
                 Terminal::move_down(1)?;
+                self.cursor_y += 1;
             }
             (KeyCode::Char('k') | KeyCode::Down) => {
                 Terminal::move_up(1)?;
+
+                self.cursor_y = self.cursor_y.saturating_sub(1);
             }
             (KeyCode::Char('l') | KeyCode::Up) => {
                 Terminal::move_right(1)?;
+                self.cursor_x += 1;
             }
             _ => {}
         }
+        self.update_top_offset();
         Ok(())
     }
 
-    fn open_file(file_name: &str) -> Result<Vec<String>, Error> {
+    fn update_top_offset(&mut self) {
+        let height = self.term.height;
+        if self.cursor_y < self.top_offset {
+            self.top_offset = self.cursor_y;
+        } else if self.cursor_y >= self.top_offset + height {
+            self.top_offset = self.cursor_y - height + 1;
+        }
+    }
+
+    fn open_file(file_name: &str) -> Result<Document, Error> {
         let file = fs::read_to_string(file_name)?;
-        Ok(file.lines().map(str::to_string).collect())
+        let lines: Vec<String> = file.lines().map(str::to_string).collect();
+        let n_lines = lines.len() as u16;
+        Ok(Document { lines, n_lines })
     }
 
     fn render(&self) -> Result<(), Error> {
-        for (i, line) in self.docu.iter().enumerate() {
-            Terminal::move_cursor(0, i as u16)?;
-            Terminal::print(line)?;
+        let n_lines = self.docu.n_lines;
+        let height = self.term.height;
+        let width = self.term.width;
+
+        for row in 0..height {
+            Terminal::move_cursor(0, row)?;
+            let doc_row = self.top_offset + row;
+            if doc_row < n_lines {
+                let line = &self.docu.lines[doc_row as usize];
+                Terminal::print(&line.chars().take(width as usize).collect::<String>())?;
+            } else {
+                Terminal::print("~")?;
+            }
         }
+
+        let cursor_screen_y = self.cursor_y.saturating_sub(self.top_offset);
+        Terminal::move_cursor(self.cursor_x, cursor_screen_y)?;
+
+        Terminal::flush()?;
+
         Ok(())
     }
 }
